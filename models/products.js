80f6968e71e822000f95a30e7c58097e963e8026
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-08-23 00:48:26 -0300

    feat (IndexJs): setting up project and connecting to database

---
+++ models/products.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema({
+  code: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Products", productSchema);
