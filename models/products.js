9c17ce4703c9363646bb6a98bafde4b08331a473
Christian de Souza Pires <christianyourdan1@gmail.com>
2024-08-23 01:45:30 -0300

    feat (AddProductsPage): creating add products page and table in home page

--- models/products.js
+++ models/products.js
@@ -8,12 +8,12 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  price: {
-    type: Number,
-    required: true,
-  },
   description: {
     type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
     required: true,
   },
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/products", (req, res) => {
-  res.send("All Products");
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
+router.get("/add", (req, res) => {
+  res.render("add_products", { title: "Add Products" });
 });
 
 module.exports = router;
